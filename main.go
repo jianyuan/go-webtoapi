28087b7150efaff526e9e7ac92f377411b713597
Jian Yuan Lee <jianyuan@gmail.com>
2019-02-11 00:31:01 +0000

    Refactor

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 )
+
+func selectionFromResolveParams(p graphql.ResolveParams) (*goquery.Selection, bool) {
+	selection, ok := p.Source.(*goquery.Selection)
+	if ok {
+		selector, ok := p.Args["selector"].(string)
+		if ok {
+			selection = selection.Find(selector)
+		}
+		return selection, true
+	}
+	return nil, false
+}
 
 func newSelectorField(fn func(*goquery.Selection) (interface{}, error)) *graphql.Field {
 	return &graphql.Field{
@@ -17,12 +30,8 @@ func newSelectorField(fn func(*goquery.Selection) (interface{}, error)) *graphql
 			},
 		},
 		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-			selection, ok := p.Source.(*goquery.Selection)
+			selection, ok := selectionFromResolveParams(p)
 			if ok {
-				selector, ok := p.Args["selector"].(string)
-				if ok {
-					selection = selection.Find(selector)
-				}
 				return fn(selection)
 			}
 			return nil, nil
@@ -30,25 +39,124 @@ func newSelectorField(fn func(*goquery.Selection) (interface{}, error)) *graphql
 	}
 }
 
-var DocumentType = graphql.NewObject(graphql.ObjectConfig{
-	Name: "Document",
+var NodeType = graphql.NewInterface(graphql.InterfaceConfig{
+	Name: "Node",
 	Fields: graphql.Fields{
-		"content": newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
-			return selection.Html()
-		}),
-		"html": newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
-			return goquery.OuterHtml(selection)
-		}),
-		"text": newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
-			return selection.Text(), nil
-		}),
-		"tag": newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
-			return goquery.NodeName(selection), nil
-		}),
+		"content": &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				"selector": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+		},
+		"html": &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				"selector": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+		},
+		"text": &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				"selector": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+		},
+		"tag": &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				"selector": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+		},
+		"attr": &graphql.Field{
+			Type: graphql.String,
+			Args: graphql.FieldConfigArgument{
+				"selector": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"name": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+		},
 	},
 })
 
+func addNodeFieldConfigs(gt *graphql.Object) {
+	gt.AddFieldConfig("content", newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
+		return selection.Html()
+	}))
+	gt.AddFieldConfig("html", newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
+		return goquery.OuterHtml(selection)
+	}))
+	gt.AddFieldConfig("text", newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
+		return selection.Text(), nil
+	}))
+	gt.AddFieldConfig("tag", newSelectorField(func(selection *goquery.Selection) (interface{}, error) {
+		return goquery.NodeName(selection), nil
+	}))
+	gt.AddFieldConfig("attr", &graphql.Field{
+		Type: graphql.String,
+		Args: graphql.FieldConfigArgument{
+			"selector": &graphql.ArgumentConfig{
+				Type: graphql.String,
+			},
+			"name": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		},
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			selection, ok := selectionFromResolveParams(p)
+			if ok {
+				name, ok := p.Args["name"].(string)
+				if ok {
+					val, exists := selection.Attr(name)
+					if exists {
+						return val, nil
+					}
+				}
+			}
+			return nil, nil
+		},
+	})
+}
+
+var DocumentType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Document",
+	Interfaces: []*graphql.Interface{
+		NodeType,
+	},
+	Fields: graphql.Fields{
+		"title": &graphql.Field{
+			Type: graphql.String,
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				selection, ok := p.Source.(*goquery.Selection)
+				if ok {
+					return selection.Find("title").Text(), nil
+				}
+				return nil, nil
+			},
+		},
+	},
+})
+
+var ElementType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Element",
+	Interfaces: []*graphql.Interface{
+		NodeType,
+	},
+	Fields: graphql.Fields{},
+})
+
 func main() {
+	addNodeFieldConfigs(DocumentType)
+
 	fields := graphql.Fields{
 		"page": &graphql.Field{
 			Type: DocumentType,
@@ -79,7 +187,11 @@ func main() {
 	}
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
 	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
-	schema, _ := graphql.NewSchema(schemaConfig)
+	schema, err := graphql.NewSchema(schemaConfig)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	h := handler.New(&handler.Config{
 		Schema:   &schema,
